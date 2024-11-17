adf56e5e9d5fcad740fa46ffd4511f9778f34d73
Gustavo Braga <gustavo.rbraga98@gmail.com>
2024-11-17 20:17:31 -0300

    deleting trash and organizing functions

--- ekf_slam/agora_vai.py
+++ ekf_slam/agora_vai.py
@@ -153,22 +153,6 @@ class OdomNoiseNode(Node):
         if self.leituras_ignoradas >= self.leituras_para_ignorar:
             self.get_logger().debug("Atualizando mapa com LIDAR")
             self.atualizar_mapa_lidar(scan_msg, self.posicao_robô_x, self.posicao_robô_y)
-
-    def kalman_predict(self, state, covariance, control_input, motion_noise):
-        """
-        Fase de predição do Filtro de Kalman.
-        Prediz o estado baseado no controle aplicado.
-        """
-        # Atualizar o estado com o controle
-        state = state + control_input
-
-        # Modelo de movimento - linear aproximação
-        F = np.eye(3)  # Matriz de transição de estado
-
-        # Atualização da covariância
-        covariance = F @ covariance @ F.T + motion_noise
-        
-        return state, covariance
     
     def localizar_posicao_global(self, odom_msg):
         # Transformar odometria, SAX proxy for calcular a posição global mais precisa
@@ -178,32 +162,6 @@ class OdomNoiseNode(Node):
         orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
         (_, _, yaw) = self.euler_from_quaternion(orientation_list)
         return yaw
-
-    def kalman_update(self, state, covariance, z, R):
-        """
-        Fase de correção do Filtro de Kalman.
-        'z' são as medições do LiDAR; 'R' é a matriz de ruído da observação.
-        """
-        # Atualize H para refletir somente as estimativas de x e y
-        H = np.eye(3)[:2, :]  # Selecionando apenas os dois primeiros componentes do estado para a atualização
-        
-        # Calcular o erro de inovação somente no espaço de observação
-        y = z - H @ state
-
-        # Calcular a matriz de inovação
-        S = H @ covariance @ H.T + R
-
-        # Calcular o ganho de Kalman
-        K = covariance @ H.T @ np.linalg.inv(S)
-
-        # Atualizar o estado
-        state = state + K @ y
-
-        # Atualizar a covariância
-        I = np.eye(covariance.shape[0])
-        covariance = (I - K @ H) @ covariance
-
-        return state, covariance
 
     def transformar_coordenadas_para_grid(self, x_mundo_real, y_mundo_real):
         x_grid = int(x_mundo_real / self.grid_res) + self.origin_x
